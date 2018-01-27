952b8421cef816ec44e860b5760b75e00c504a47
KaFai Choi <kafaicoder@gmail.com>
2018-01-27 17:00:16 +0700

    add icons

--- apps/vegsochk_web/assets/js/pages/animals.js
+++ apps/vegsochk_web/assets/js/pages/animals.js
@@ -1,12 +1,14 @@
-const hideElement = element =>
+const hideElement = element => {
   element.style.display = 'none'
+}
 
-const showElement = element =>
+const showElement = element => {
   element.style.display = 'block'
+}
 
 const hideAllChildren = element => {
-  const children = element.children;
-  var i;
+  const children = element.children
+  var i
   for (i = 0; i < children.length; i++) {
     hideElement(children[i])
   }
@@ -17,22 +19,21 @@ const setOverlayContentHandler = (overlayContainer, overlayContentContainer, id)
   const overlayElement = document.getElementById(id + '-overlay')
 
   element.onclick = () => {
-    overlayContainer.style.width = "100%";
-    hideAllChildren(overlayContentContainer);
-    showElement(overlayElement);
+    overlayContainer.style.width = '100%'
+    hideAllChildren(overlayContentContainer)
+    showElement(overlayElement)
   }
 }
 
 const bootstrap = () => {
-  const overlayContainer = document.getElementById('overlay-container');
-  const overlayContentContainer = document.getElementById('overlay-content-container');
-  const closeOverlayButton = document.getElementById('overlay-close');
+  const overlayContainer = document.getElementById('overlay-container')
+  const overlayContentContainer = document.getElementById('overlay-content-container')
+  const closeOverlayButton = document.getElementById('overlay-close')
   closeOverlayButton.onclick = () => {
-    overlayContainer.style.width = "0%";
+    overlayContainer.style.width = '0%'
   }
-  const ids = ['pig', 'cow', 'chicken', 'chick', 'fish'];
-  ids.forEach(setOverlayContentHandler.bind(null, overlayContainer, overlayContentContainer));
+  const ids = ['pig', 'cow', 'chicken', 'chick', 'fish']
+  ids.forEach(setOverlayContentHandler.bind(null, overlayContainer, overlayContentContainer))
 }
 
-
-bootstrap();
+bootstrap()
