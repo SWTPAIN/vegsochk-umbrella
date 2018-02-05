ba5d5ba4afa30f36b249301f7601360f56a537ef
KaFai Choi <kafaicoder@gmail.com>
2018-02-05 22:46:18 +0700

    fix bug in render 404

--- apps/vegsochk_web/lib/vegsochk_web/views/error_view.ex
+++ apps/vegsochk_web/lib/vegsochk_web/views/error_view.ex
@@ -1,17 +1,17 @@
 defmodule VegsochkWeb.ErrorView do
   use VegsochkWeb, :view
 
-  def render("404.html", _assigns) do
-    render("404.html", layout: {VegsochkWeb.LayoutView, "article.html"})
+  def render("404.html", assigns) do
+    render("404_page.html", conn: assigns.conn)
   end
 
   def render("500.html", _assigns) do
-    render("500.html", layout: {VegsochkWeb.LayoutView, "article.html"})
+    render("500_page.html")
   end
 
   # In case no render clause matches or no
   # template is found, let's render it as 500
-  def template_not_found(_template, _assigns) do
-    render("500.html", layout: {VegsochkWeb.LayoutView, "article.html"})
+  def template_not_found(_template, assigns) do
+    render("500.html", assigns)
   end
 end
