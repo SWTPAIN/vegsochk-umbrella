dd36b7a51eb7f3fcdfb1f618590a16d0546c4d08
KaFai Choi <kafaicoder@gmail.com>
2018-04-22 14:54:38 +0800

    add ga

--- apps/vegsochk_web/lib/vegsochk_web/views/layout_view.ex
+++ apps/vegsochk_web/lib/vegsochk_web/views/layout_view.ex
@@ -11,4 +11,18 @@ defmodule VegsochkWeb.LayoutView do
 
 	def ogtags(_), do: nil
 
+  def ga_tag do
+    raw """
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-117938479-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'UA-117938479-1');
+    </script>
+    """
+  end
+
 end
