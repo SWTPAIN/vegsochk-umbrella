771c2242ea642c82bab6ef2e02138f69e9460611
KaFai Choi <kafaicoder@gmail.com>
2018-12-08 23:09:58 +0700

    upgrade babel to 7

--- apps/vegsochk_web/assets/webpack.config.js
+++ apps/vegsochk_web/assets/webpack.config.js
@@ -1,62 +1,67 @@
-var path = require('path')
-var ExtractTextPlugin = require('extract-text-webpack-plugin')
-var CopyWebpackPlugin = require('copy-webpack-plugin')
-var webpack = require('webpack')
-var env = process.env.MIX_ENV || 'dev'
-var isProduction = (env === 'prod')
+var path = require("path");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var CopyWebpackPlugin = require("copy-webpack-plugin");
+var webpack = require("webpack");
+var env = process.env.MIX_ENV || "dev";
+var isProduction = env === "prod";
 
 module.exports = {
   entry: {
-    'app': ['./js/app.js', './css/app.scss'],
-    'pages/authors-articles-new': ['./js/pages/authors/articles/new.js'],
-    'pages/authors-articles-edit': ['./js/pages/authors/articles/edit.js'],
-    'animals': ['./js/pages/animals.js'],
-    'vegan-tips': ['./js/pages/vegan-tips.js']
+    app: ["./js/app.js", "./css/app.scss"],
+    "pages/authors-articles-new": ["./js/pages/authors/articles/new.js"],
+    "pages/authors-articles-edit": ["./js/pages/authors/articles/edit.js"],
+    animals: ["./js/pages/animals.js"],
+    "vegan-tips": ["./js/pages/vegan-tips.js"]
   },
   output: {
-    path: path.resolve(__dirname, '../priv/static/'),
-    filename: 'js/[name].js'
+    path: path.resolve(__dirname, "../priv/static/"),
+    filename: "js/[name].js"
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: [".js", ".jsx"]
   },
   module: {
-    rules: [{
-      test: /\.(sass|scss)$/,
-      include: /css/,
-      use: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
+    rules: [
+      {
+        test: /\.(sass|scss)$/,
+        include: /css/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [
+            { loader: "css-loader" },
+            {
+              loader: "sass-loader",
+              options: {
+                includePaths: [path.resolve("node_modules/bootstrap/scss")],
+                sourceComments: !isProduction
+              }
+            }
+          ]
+        })
+      },
+      {
+        test: /\.(js|jsx)$/,
+        include: /js/,
         use: [
-          {loader: 'css-loader'},
           {
-            loader: 'sass-loader',
+            loader: "babel-loader",
             options: {
-              includePaths: [
-                path.resolve('node_modules/bootstrap/scss')
-              ],
-              sourceComments: !isProduction
+              presets: ["@babel/preset-env", "@babel/preset-react"]
             }
           }
         ]
-      })
-    },
-    {
-      test: /\.(js|jsx)$/,
-      include: /js/,
-      use: [
-        { loader: 'babel-loader' }
-      ]
-    }]
+      }
+    ]
   },
   plugins: [
-    new CopyWebpackPlugin([{ from: './static' }]),
-    new ExtractTextPlugin('css/app.css'),
+    new CopyWebpackPlugin([{ from: "./static" }]),
+    new ExtractTextPlugin("css/app.css"),
     new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      'window.jQuery': 'jquery',
-      Popper: ['popper.js', 'default']
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery",
+      Popper: ["popper.js", "default"]
     })
   ]
-}
+};
